1367fd769245510798461b5ed341b51f82f9ad5b
MiloMarchettiGR <mi.marchetti@goreply.io>
2025-02-05 16:00:41 +0100

    Add script to create env file

---
+++ src/create_env_file.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -eE -o functrace
+
+fatal() {
+  local LINE="$1"
+  local CMD="$2"
+  echo "[FATAL] $LINE: $CMD"
+  exit 1
+}
+
+trap 'fatal "$LINENO" "$BASH_COMMAND"' ERR
+
+create_env_file() {
+
+  # Create or overwrite .env file
+  echo "# Environment variables generated from $VARIABLES" > "$OUTPUT_NAME"
+
+  if [[ -n $VARIABLES ]]; then 
+    # Extract variables and write them to .env
+    jq -r 'to_entries | .[] | "\(.key)=\(.value)"' "$VARIABLES" >> "$OUTPUT_NAME"
+  fi
+
+  if [[ -n $SECRETS ]]; then 
+    # Extract secrets and write them to .env
+    jq -r 'to_entries | .[] | "\(.key)=\(.value)"' "$SECRETS" >> "$OUTPUT_NAME"
+  fi
+
+    echo "$OUTPUT_NAME file has been created successfully!"
+}
+
+create_env_file
