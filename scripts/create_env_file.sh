1a491c7ac9311f21f89b1f9dd252c03297b3780c
MiloMarchettiGR <mi.marchetti@goreply.io>
2025-02-05 17:13:09 +0100

    Fix create_env_file

--- scripts/create_env_file.sh
+++ scripts/create_env_file.sh
@@ -15,20 +15,21 @@ create_env_file() {
 
   # Create or overwrite .env file
   touch "$OUTPUT_NAME"
+  chmod -R 777 "$OUTPUT_NAME"
   ls -lart
 
   if [[ -n $VARIABLES ]]; then 
     # Extract variables and write them to .env
     echo "$VARIABLES"
     JSON_STRING='{"PROJECT_ID":"project_id","SERVICE_ACCOUNT":"serv"}'
-    echo "$JSON_STRING" | jq -r 'to_entries | .[] | "\(.key)=\(.value)"' >> "$OUTPUT_ENV"
+    echo "$JSON_STRING" | jq -c -r 'to_entries | .[] | "\(.key)=\(.value)"' >> "$OUTPUT_ENV"
   fi
 
   if [[ -n $SECRETS ]]; then 
     # Extract secrets and write them to .env
     echo "$SECRETS"
     JSON_STRING='{"PROJECT_ID":"project_id","SERVICE_ACCOUNT":"serv"}'
-    echo "$JSON_STRING" | jq -r 'to_entries | .[] | "\(.key)=\(.value)"' >> "$OUTPUT_ENV"
+    echo "$JSON_STRING" | jq -c -r 'to_entries | .[] | "\(.key)=\(.value)"' >> "$OUTPUT_ENV"
   fi
 
     echo "$OUTPUT_NAME file has been created successfully!"
