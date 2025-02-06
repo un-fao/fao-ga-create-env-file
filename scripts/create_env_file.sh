6119ae8ff0e1fc2bf2150ed19b39eb1e213ac643
MiloMarchettiGR <mi.marchetti@goreply.io>
2025-02-06 11:47:53 +0100

    Test quotes handling

--- scripts/create_env_file.sh
+++ scripts/create_env_file.sh
@@ -17,7 +17,7 @@ create_env_file() {
 
   if [[ -n $VARIABLES ]]; then 
     # Extract variables and write them to .env with conditional quotes
-    echo "$VARIABLES" | jq -c -r 'to_entries | .[] | if (.value | test("\\s")) then "\(.key)=\"\(.value)\"" else "\(.key)=\(.value)" end' >> "$OUTPUT_NAME"
+    echo "$VARIABLES" | jq -c -r "to_entries | .[] | if (.value | test('\\s')) then '\(.key)=\'\(.value)\'' else '\(.key)=\(.value)' end" >> "$OUTPUT_NAME"
   fi
 
   # Add backslash before each quote or double quote
