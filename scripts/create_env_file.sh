66772ce717f1b71a8d93f2d0ebc50c6757f5dd8f
MiloMarchettiGR <mi.marchetti@goreply.io>
2025-02-06 11:53:55 +0100

    Test quotes handling

--- scripts/create_env_file.sh
+++ scripts/create_env_file.sh
@@ -12,16 +12,18 @@ fatal() {
 trap 'fatal "$LINENO" "$BASH_COMMAND"' ERR
 
 create_env_file() {
+  echo $VARIABLES
+  # Escape quotes in VARIABLES
+  VARIABLES=$(echo "$VARIABLES" | sed 's/"/\\"/g')
+  echo $VARIABLES
+
   # Create or overwrite .env file
   touch "$OUTPUT_NAME"
 
   if [[ -n $VARIABLES ]]; then 
     # Extract variables and write them to .env with conditional quotes
-    echo "$VARIABLES" | jq -c -r "to_entries | .[] | if (.value | test('\\s')) then '\(.key)=\'\(.value)\'' else '\(.key)=\(.value)' end" >> "$OUTPUT_NAME"
+    echo "$VARIABLES" | jq -c -r 'to_entries | .[] | if (.value | test("\\s")) then "\(.key)='\(.value)'" else "\(.key)=\(.value)" end' >> "$OUTPUT_NAME"
   fi
-
-  # Add backslash before each quote or double quote
-  sed -i 's/\"/\\\"/g' "$OUTPUT_NAME"
 
   echo "$OUTPUT_NAME file has been created successfully!"
 }
