4ad5492eb8904781f6746f5f62a93c17c0089aa6
MiloMarchettiGR <mi.marchetti@goreply.io>
2025-02-10 10:33:29 +0100

    Add secrets handling

--- scripts/create_env_file.sh
+++ scripts/create_env_file.sh
@@ -12,23 +12,39 @@ fatal() {
 trap 'fatal "$LINENO" "$BASH_COMMAND"' ERR
 
 create_env_file() {
-  # Create or overwrite .env file
-  touch "$OUTPUT_NAME"
+  local FILE_NAME = "$1"
+  # Check if the file exists
+  if [ -e "OUTPUT_NAME" ]; then
+    echo "[FAILED]"
+    echo "File '$OUTPUT_NAME' already exists."
+  else
+    # Create the file
+    touch "$OUTPUT_NAME"
+    echo "File '$OUTPUT_NAME' created successfully."
+  fi
+}
 
-  if [ -z ${VARIABLES+x} ] || [ "$VARIABLES" = "null" ]; then
-    echo "$OUTPUT_NAME file has been created but no variables where found."
+add_env_variables() {
+  local ENV_VARIABLES = "$1"
+  local NAME = "$2"
+  local FILE_NAME = "$3"
+
+  if [ -z ${ENV_VARIABLES+x} ] || [ "$ENV_VARIABLES" = "null" ]; then
+    echo "No $NAME where found."
     return
   fi
 
   # Map variables to .env file, add delimenters in case of space separated values
-  echo "$VARIABLES" | jq -c -r 'to_entries | .[] | if (.value | test("\\s")) then "\(.key)=¦\(.value)¦" else "\(.key)=\(.value)" end' >> "$OUTPUT_NAME"
+  echo "$ENV_VARIABLES" | jq -c -r 'to_entries | .[] | if (.value | test("\\s")) then "\(.key)=¦\(.value)¦" else "\(.key)=\(.value)" end' >> "$FILE_NAME"
   # Add backslash before double quotes and quotes
-  sed -i 's/\"/\\\"/g' "$OUTPUT_NAME"
-  sed -i "s/'/\\\'/g" "$OUTPUT_NAME"
+  sed -i 's/\"/\\\"/g' "$FILE_NAME"
+  sed -i "s/'/\\\'/g" "$FILE_NAME"
   # Replace delimeters with double quotes
-  sed -i 's/¦/\"/g' "$OUTPUT_NAME"
+  sed -i 's/¦/\"/g' "$FILE_NAME"
 
-  echo "$OUTPUT_NAME file has been created successfully!"
+  echo "$NAME added to $FILE_NAME!"
 }
 
-create_env_file
+create_env_file "$OUTPUT_NAME"
+add_variables "$VARIABLES" "variables" "$OUTPUT_NAME"
+add_variables "$SECRETS" "secrets" "$OUTPUT_NAME"
